157e1cd90efe53f638380d20985a54fdb4448cc7
sanbei101 <sanbei101@outlook.com>
2026-05-09 15:45:13 +0800

    fix: 在 GatewayPullTask 中设置任务的 StreamID

--- internal/db/redis.go
+++ internal/db/redis.go
@@ -128,6 +128,7 @@ func (r *Redis) GatewayPullTask(ctx context.Context, batch int64) ([]*GatewayPus
 			ReleaseGatewayPushTask(task)
 			continue
 		}
+		task.StreamID = msg.ID
 		tasks = append(tasks, task)
 	}
 	return tasks, nil
